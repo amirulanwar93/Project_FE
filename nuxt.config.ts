f640b37152fd0d1912c99bb2917b4849f359001d
Amirul Anwar <anwar_solong@yahoo.com.my>
2023-11-13 23:48:36 +0800

    major update
    -structure html on user page 90%
    -add prettier plugin tailwindcss (still not working)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
 })
+
